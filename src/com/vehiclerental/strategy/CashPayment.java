ce2267809a71b2c76c5f8ba723868ccfe8aa8f71
Zayniddin <zayniddinjonoo11@gmail.com>
2025-11-16 18:26:39 +0500

    Finall update

--- src/com/vehiclerental/strategy/CashPayment.java
+++ src/com/vehiclerental/strategy/CashPayment.java
@@ -7,7 +7,7 @@ public class CashPayment implements IPaymentStrategy {
     @Override
     public boolean pay(double amount, Scanner scanner) {
         System.out.println("Processing Cash payment of " + amount + "...");
-        System.out.println("✅ Payment successful!");
+        System.out.println(" Payment successful!");
         return true;
     }
 }
